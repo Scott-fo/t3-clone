4de1eeba5e24bcabcbde901bb686bf614181ea41
Scott <scottfo@live.com>
2025-06-08 14:28:18 +0100

    add telem

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use axum::{Router, routing::get};
+use t3_clone::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() {
-    tracing_subscriber::fmt::init();
+async fn main() -> Result<(), std::io::Error> {
+    let subscriber = get_subscriber(
+        "t3_clone".into(),
+        "info,t3_clone=info".into(),
+        std::io::stdout,
+    );
+    init_subscriber(subscriber);
 
-    let app = Router::new().route("/", get(root));
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn root() -> &'static str {
-    "Hello"
+    Ok(())
 }
