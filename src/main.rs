8f620ec7c63086a821b82a61ed4ba32251410040
Scott <scottfo@live.com>
2025-06-08 20:37:20 +0100

    run app in main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use t3_clone::{
+    app::Application,
     configuration::get_configuration,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -13,6 +14,11 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read config");
+    let app = Application::build(config.clone())
+        .await
+        .expect("Failed to create application");
+
+    app.run_until_stopped().await?;
 
     Ok(())
 }
