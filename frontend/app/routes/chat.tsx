89764888a3ae6daf3e35d2050093821324706116
Scott <scottfo@live.com>
2025-06-15 11:04:07 +0100

    revert max w changes

--- frontend/app/routes/chat.tsx
+++ frontend/app/routes/chat.tsx
@@ -138,7 +138,7 @@ export default function Page({ params }: Route.ComponentProps) {
       />
       <div
         ref={containerRef}
-        className="max-w-3xl flex-1 overflow-y-auto overflow-x-none px-4 py-4 space-y-10 custom-scrollbar"
+        className="flex-1 overflow-y-auto overflow-x-none px-4 py-4 space-y-10 custom-scrollbar"
       >
         {showMessages && <MessageList messages={messages} />}
         {showMessages && isPending && (
